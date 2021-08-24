6a55288e203b988e72406d0dcc21a4f977119f00
BrunoJun <brunojun7@gmail.com>
2021-08-24 14:51:42 -0300

    Implement button

---
+++ calculadora/src/org/me/calc/visao/Botao.java
@@ -0,0 +1,21 @@
+package org.me.calc.visao;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.plaf.FontUIResource;
+import java.awt.Font;
+
+import java.awt.Color;
+
+public class Botao extends JButton{
+    
+    public Botao(String texto, Color cor) {
+        
+        setText(texto);
+        setForeground(Color.WHITE);
+        setFont(new FontUIResource("courier", Font.PLAIN, 15));
+        setOpaque(true);
+        setBackground(cor);
+        setBorder(BorderFactory.createLineBorder(Color.BLACK));
+    }
+}
